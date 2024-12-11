8e960ca14979b5ea62e44fff9027e8ac5d1baf31
Angelo <angvil02@gmail.com>
2024-12-11 09:26:01 -0800

    adjusted exercise 1 variables

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
-Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
-Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
